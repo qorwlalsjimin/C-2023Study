4b82a6f60ae69e3b4f66e9eaa19a0b44fadf8868
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-03 12:02:59 +0900

    #1 Hello World를 콘솔에 출력

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic; //안 쓰는 건 컴파일할때 알아서 빼준다
 
-namespace HelloWorld //package
+namespace HelloWorld //묶어주는 용도
 {
-    internal class Program
+    internal class Program //internal: 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wolrd");
+            Console.WriteLine("안녕 세상아");
             
         }
     }
